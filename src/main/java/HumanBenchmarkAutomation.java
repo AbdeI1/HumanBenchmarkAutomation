e522ad657fd6099dfab43fcfd946d631c98d2357
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2023-06-09 17:11:04 -0500

    initial project

---
+++ src/main/java/HumanBenchmarkAutomation.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+
+public class HumanBenchmarkAutomation {
+  public static WebDriver driver;
+  public static void main(String[] args) {
+    System.setProperty("webdriver", "./drivers/chromedriver.exe");
+  }
+}
